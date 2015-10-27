d7bff2ae727a9597f9430d2b96de8802c2bfee6f
amccormack10 <aidan.mccormack@neueda.com>
2015-10-27 09:24:03 +0000

    Pie Chart
    Example of Pie Chart

---
+++ PieChart_Example/PieChart/PieChart/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieChart
+{
+    public class MainViewModel
+    {
+        private readonly ObservableCollection<Population> _populations = new ObservableCollection<Population>();
+        public ObservableCollection<Population> Populations
+        {
+            get
+            {
+                return _populations;
+            }
+        }
+
+        public MainViewModel()
+        {
+            _populations.Add(new Population() { Name = "Aidan", Count = 1340 });
+            _populations.Add(new Population() { Name = "Pat", Count = 1220 });
+            _populations.Add(new Population() { Name = "THE BIG FELLA", Count = 4000 });
+
+        }
+    }
+}
